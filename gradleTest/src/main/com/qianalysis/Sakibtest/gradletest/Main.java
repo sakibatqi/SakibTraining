a0ec50568ca811e4505929c7a628528761e5c147
sakibatqi <sakib@qianalysis.com>
2015-10-18 21:35:34 +0600

    A dependency added

--- gradleTest/src/main/com/qianalysis/Sakibtest/gradletest/Main.java
+++ gradleTest/src/main/com/qianalysis/Sakibtest/gradletest/Main.java
@@ -1,11 +1,12 @@
 package com.qianalysis.Sakibtest.gradletest;
 
+import org.apache.commons.lang3.*;
 import java.util.Arrays;
 
 public class Main{
 	public static void main(String[] args){
 		System.out.println("hello world!");
-
+		System.out.println(StringUtils.swapCase(Arrays.toString(args)));
 	}		
 
 }
