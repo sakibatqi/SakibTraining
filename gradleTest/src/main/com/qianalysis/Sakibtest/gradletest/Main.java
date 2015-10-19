15668d163de12f116099947307c0ce581414d2dd
sakibatqi <sakib@qianalysis.com>
2015-10-19 16:43:40 +0600

    multiply branch

--- gradleTest/src/main/com/qianalysis/Sakibtest/gradletest/Main.java
+++ gradleTest/src/main/com/qianalysis/Sakibtest/gradletest/Main.java
@@ -2,11 +2,25 @@ package com.qianalysis.Sakibtest.gradletest;
 
 import org.apache.commons.lang3.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
-public class Main{
+public class Main extends Multiply{
 	public static void main(String[] args){
+		Scanner in=new Scanner(System.in);
 		System.out.println("hello world!");
+		int a=in.nextInt();
+		int b=in.nextInt();
+		mul(a,b);
 		System.out.println(StringUtils.swapCase(Arrays.toString(args)));
 	}		
+
+}
+
+class Multiply{
+	static void mul(int a,int b){
+		int n=a*b;
+		System.out.println("ans "+n);
+
+	}
 
 }
